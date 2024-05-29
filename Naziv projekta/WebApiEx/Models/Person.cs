10cf2f4ab5af5bde0b7508b554ed88490904d41c
David Bolic <davidbolic03@gmail.com>
2024-05-29 11:34:08 +0200

    tested and redy

--- Naziv projekta/WebApiEx/Models/Person.cs
+++ Naziv projekta/WebApiEx/Models/Person.cs
@@ -5,10 +5,10 @@ namespace WebApiEx
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string? Name { get; set; }
+        public string?Surname { get; set; }
         public int Age { get; set; }
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
     }
 
 }
